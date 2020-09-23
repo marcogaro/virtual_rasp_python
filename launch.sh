0b09585448c1c48267da487d6211331a323fea03
marcogaro <68847387+marcogaro@users.noreply.github.com>
2020-09-23 13:50:05 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -24,16 +24,19 @@ else
 	exit;
 fi
    
-pass="example.py"
+pass="examplenuovo.py"
 
-
+echo "$nome"
+echo "$pass"
+   
+   
 lxc list  -c n | grep -w $nome
 result=$?
 echo "result: " $result 
  
 if [ $result -eq 0  ]; then
 	echo "virtual rasp already exists";
-	./destroy.sh $nome;
+	./destroynuovo.sh $nome;
 else 
 	echo "virtual rasp doesn't exist"; 
 fi
@@ -41,15 +44,16 @@ fi
    
 lxc list   
   
+#da qui creo container
 echo "Creating virtual rasp "$nome"!"
 
 lxc launch ubuntu:16.04 "$nome"
-
-lxc exec "$nome" -- addgroup gpio
 sleep 20
-lxc exec "$nome" -- usermod -a -G gpio ubuntu
-sleep 1
+lxc exec "$nome" -- sudo --login --user ubuntu cat /etc/group
+lxc exec "$nome" -- sudo --login --user ubuntu cat /etc/group
 
+
+sleep 3
 sudo mkdir -p /gpio_mnt/"$nome"
 sudo chmod 777 -R /gpio_mnt/"$nome"
 sudo mkdir -p /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio
@@ -57,14 +61,36 @@ sudo mkdir -p /gpio_mnt/"$nome"/sys/class/gpio
 
 sudo mkdir -p /gpio_mnt/"$nome"/sys/devices/platform/soc/soc\:firmware/soc\:firmware\:expgpio/gpio/gpiochip504/
 
+sleep 3
+
+
+
+
+lxc exec "$nome" -- addgroup gpio
+sleep 20
+lxc exec "$nome" -- usermod -a -G gpio ubuntu
+sleep 1
+
+lxc exec "$nome" -- sudo --login --user ubuntu cat /etc/group
+
+
+
+
 
 lxc exec "$nome" -- mkdir -p /gpio_mnt/sys/class/gpio
 lxc exec "$nome" -- mkdir -p /gpio_mnt/sys/devices/platform/soc/3f200000.gpio
+
 lxc exec "$nome" -- mkdir -p /gpio_mnt/sys/devices/platform/soc/soc\:firmware/soc\:firmware\:expgpio/gpio/gpiochip504/
+
 lxc exec "$nome" -- mkdir -p /gpio_mnt/sys/firmware/devicetree/base/soc/gpio@7e200000/
+
 lxc exec "$nome" -- mkdir -p /gpio_mnt/sys/bus/gpio/
+#######################################
 lxc exec "$nome" -- mkdir -p /gpio_mnt/sys/bus/platform/drivers/
 lxc exec "$nome" -- mkdir -p /gpio_mnt/sys/bus/platform/
+
+
+
 
 sudo chmod -R 777 /gpio_mnt/"$nome"
 
@@ -72,15 +98,27 @@ lxc config set "$nome" security.privileged true
 lxc restart "$nome"
 
 
+
+
 lxc config device add "$nome" gpiomnt disk source=/gpio_mnt/"$nome"/sys/class/gpio path=/gpio_mnt/sys/class/gpio
 lxc config device add "$nome" devicesmnt disk source=/gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio path=/gpio_mnt/sys/devices/platform/soc/3f200000.gpio
+
 lxc config device add "$nome" socmnt disk source=/sys/devices/platform/soc/soc\:firmware/soc\:firmware\:expgpio/gpio/gpiochip504/ path=/gpio_mnt/sys/devices/platform/soc/soc\:firmware/soc\:firmware\:expgpio/gpio/gpiochip504/
 
+
+#così in /sys/class/gpio vedo quello che vedrei su /gpio_mnt...
 lxc config device add "$nome" gpio disk source=/gpio_mnt/"$nome"/sys/class/gpio path=/sys/class/gpio
 lxc config device add "$nome" devices disk source=/gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio path=/sys/devices/platform/soc/3f200000.gpio
+
 lxc config device add "$nome" soc disk source=/sys/devices/platform/soc/soc\:firmware/soc\:firmware\:expgpio/gpio/gpiochip504/ path=/sys/devices/platform/soc/soc\:firmware/soc\:firmware\:expgpio/gpio/gpiochip504/
 
+#lxc config device add "$nome" firmware disk source=/sys/firmware/devicetree/base/soc/gpio@7e200000/ path=/sys/firmware/devicetree/base/soc/gpio@7e200000/
+
+#lxc config device add "$nome" bus disk source=/sys/bus/gpio/ path=/sys/bus/gpio/
+
+
 sleep 2
+
 ls
 
 sudo chmod -R 777 /sys/class/gpio/
@@ -88,7 +126,10 @@ sudo chmod -R 777 /sys/devices/platform/soc/
 sudo chmod -R 777 /gpio_mnt/"$nome"
 sudo chmod -R 777 /gpio_mnt/"$nome"/sys/
 
-sleep 5
+sudo groupadd gpio
+sudo chgrp gpio -R /sys/class/gpio/
+
+sleep 10
 
 python3 "$pass" /sys/devices/platform/soc/3f200000.gpio /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio/ $nome &
 python3 "$pass" /sys/class/gpio/ /gpio_mnt/"$nome"/sys/class/gpio/ $nome &
@@ -96,3 +137,5 @@ python3 "$pass" /sys/class/gpio/ /gpio_mnt/"$nome"/sys/class/gpio/ $nome &
 cd
 
 lxc exec "$nome" -- su --login ubuntu -l
+
+
