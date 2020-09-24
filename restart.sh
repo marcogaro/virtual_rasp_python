0ba4e875857f184afa7461519b2c4a13f64a2b6b
marcogaro <68847387+marcogaro@users.noreply.github.com>
2020-09-24 10:18:10 +0200

    Create restart.sh

---
+++ restart.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ "$#" -eq 1 ] ; then
+    	nome=$1	
+    	echo "virtual rasp name: "$nome ;
+elif [ "$#" -eq 0 ] ; then
+	echo "virtual rasp name? "
+	read -r b
+	nome=$b ;	
+else
+   	echo "error: missing mandatory name" 
+   	exit
+fi
+
+
+echo "virtual rasp name: "$nome
+
+grep -w $nome example.ini
+result=$?
+echo "result: " $result 
+ 
+if [ $result -eq 0  ]; then
+	echo "name in example.ini";
+else 
+	echo "missing name in example.ini"; 
+	exit;
+fi
+
+
+pass="examplenuovo.py"
+echo $pass
+
+echo "restarting filesystem "$nome"!"
+
+sudo umount --force /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio
+sudo umount --force /gpio_mnt/"$nome"/sys/class/gpio
+
+python3 "$pass" /sys/devices/platform/soc/3f200000.gpio /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio/ $nome &
+python3 "$pass" /sys/class/gpio/ /gpio_mnt/"$nome"/sys/class/gpio/ $nome &
